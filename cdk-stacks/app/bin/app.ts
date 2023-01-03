f52759772a26dcf649042724cd868d65180b1c51
jviloria <jviloria96744@gmail.com>
2023-01-03 13:23:55 -0800

    Initialize cdk app stack

---
+++ cdk-stacks/app/bin/app.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { AppStack } from "../lib/app-stack";
+
+const app = new cdk.App();
+new AppStack(app, "AppStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
