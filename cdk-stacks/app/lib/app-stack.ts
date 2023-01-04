306a0f318752c18579a1015811709f36ef2340b2
jviloria <jviloria96744@gmail.com>
2023-01-03 23:29:10 -0800

    Add web client to deployment pipeline

--- cdk-stacks/app/lib/app-stack.ts
+++ cdk-stacks/app/lib/app-stack.ts
@@ -6,12 +6,25 @@ export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const domainName = "jviloria.com";
+    const subDomain = "photo-maps-app";
+    const siteBuildDirectory = "dist";
+
     const adminSiteProps: StaticSiteProps = {
-      domainName: "jviloria.com",
-      siteSubDomain: "admin-portal.photo-maps-app",
-      siteBuildDirectory: "dist",
+      domainName,
+      siteSubDomain: `admin-portal.${subDomain}`,
+      siteBuildDirectory,
       siteDirectory: "admin-portal",
     };
     new StaticSite(this, "admin-site", adminSiteProps);
+
+    const webPlatformProps: StaticSiteProps = {
+      domainName,
+      siteSubDomain: subDomain,
+      siteBuildDirectory,
+      siteDirectory: "client-web",
+    };
+
+    new StaticSite(this, "client-web", webPlatformProps);
   }
 }
