e318b8faffc200fcaca3feccb696fd8c4dbde325
jviloria <jviloria96744@gmail.com>
2023-01-04 16:35:15 -0800

    Add Cognito auth flow for admin site

---
+++ cdk-stacks/app/constructs/admin-auth-flow.ts
@@ -0,0 +1,74 @@
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import { CfnOutput, Stack, RemovalPolicy, Duration } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class AdminAuthFlow extends Construct {
+  constructor(parent: Stack, name: string) {
+    super(parent, name);
+
+    const userPool = new cognito.UserPool(this, name, {
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      userVerification: {
+        emailSubject: "Please verify your email",
+        emailBody: "Verification Code: {####}",
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+
+    const adminScope = new cognito.ResourceServerScope({
+      scopeName: "admin",
+      scopeDescription: "Admin Scope",
+    });
+
+    const resourceServer = new cognito.UserPoolResourceServer(this, name, {
+      scopes: [adminScope],
+      userPool,
+      identifier: "admin-site-resource-server",
+    });
+
+    new CfnOutput(this, "userPoolId", {
+      value: userPool.userPoolId,
+    });
+
+    const userPoolClient = new cognito.UserPoolClient(this, name, {
+      userPool,
+      accessTokenValidity: Duration.minutes(60),
+      idTokenValidity: Duration.minutes(60),
+      generateSecret: false,
+      refreshTokenValidity: Duration.days(1),
+      enableTokenRevocation: true,
+      preventUserExistenceErrors: true,
+      authFlows: {
+        userPassword: true,
+      },
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true,
+        },
+        scopes: [
+          cognito.OAuthScope.OPENID,
+          cognito.OAuthScope.resourceServer(resourceServer, adminScope),
+        ],
+        callbackUrls: ["http://localhost:5173/"],
+      },
+    });
+
+    userPool.addDomain("admin-site-domain", {
+      cognitoDomain: {
+        domainPrefix: "admin-portal",
+      },
+    });
+
+    new CfnOutput(this, "userPoolClientId", {
+      value: userPoolClient.userPoolClientId,
+    });
+  }
+}
--- cdk-stacks/app/lib/app-stack.ts
+++ cdk-stacks/app/lib/app-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { createStaticSiteProps } from "../config";
 import { Construct } from "constructs";
 import { StaticSite } from "../constructs/static-site";
+import { AdminAuthFlow } from "../constructs/admin-auth-flow";
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +13,7 @@ export class AppStack extends cdk.Stack {
 
     const webPlatformProps = createStaticSiteProps("client-web");
     new StaticSite(this, "client-web", webPlatformProps);
+
+    new AdminAuthFlow(this, "admin-auth-flow");
   }
 }
