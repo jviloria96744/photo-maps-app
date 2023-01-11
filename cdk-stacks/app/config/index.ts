74dcc87599edcc3a23b672202cfcb2ae48439c1c
jviloria <jviloria96744@gmail.com>
2023-01-11 15:57:22 -0800

    Remove dev requirements file

--- cdk-stacks/app/config/index.ts
+++ cdk-stacks/app/config/index.ts
@@ -41,5 +41,5 @@ export const webClientCallbackUrls: string[] = [
 export const lambdaBuildCommands = [
   "bash",
   "-c",
-  "pip install -r requirements.txt -t /asset-output && cp -au . /asset-output && rm -rf /asset-output/tests",
+  "pip install -r requirements.txt -t /asset-output && cp -au . /asset-output && rm -rf /asset-output/tests && rm -f requirements_dev.txt",
 ];
