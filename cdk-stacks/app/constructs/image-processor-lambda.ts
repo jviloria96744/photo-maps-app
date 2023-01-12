22729399e34eca1015c82aef36cad1f1ff682310
jviloria <jviloria96744@gmail.com>
2023-01-12 11:16:31 -0800

    Add layer build step to deployment pipeline

--- cdk-stacks/app/constructs/image-processor-lambda.ts
+++ cdk-stacks/app/constructs/image-processor-lambda.ts
@@ -1,6 +1,5 @@
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as lambdaPython from "@aws-cdk/aws-lambda-python-alpha";
 import { CfnOutput, Stack, Duration } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as path from "path";
@@ -37,8 +36,8 @@ export class ImageProcessorLambda extends Construct {
       architecture: lambda.Architecture.ARM_64,
       timeout: Duration.seconds(15),
       layers: [
-        new lambdaPython.PythonLayerVersion(parent, `${name}-layer`, {
-          entry: layerPath,
+        new lambda.LayerVersion(parent, `${name}-layer`, {
+          code: lambda.Code.fromAsset(layerPath),
           compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
           compatibleArchitectures: [lambda.Architecture.ARM_64],
         }),
