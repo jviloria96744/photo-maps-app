309ec8098b6b13b03b0d1fcc7da3d513fbef13f5
jviloria <jviloria96744@gmail.com>
2023-01-14 23:49:46 -0800

    Deploy empty stack in preparation of refactoring into nested stacks, remove redundant synth step from deployment pipeline

--- cdk-stacks/app/lib/app-stack.ts
+++ cdk-stacks/app/lib/app-stack.ts
@@ -1,27 +1,27 @@
 import * as cdk from "aws-cdk-lib";
-import { createStaticSiteProps } from "../config";
+// import { createStaticSiteProps } from "../config";
 import { Construct } from "constructs";
-import { StaticSite } from "../constructs/static-site";
-import { AdminAuthFlow } from "../constructs/admin-auth-flow";
-import { WebClientAuthFlow } from "../constructs/client-web-auth-flow";
-import { ImageProcessorLambda } from "../constructs/image-processor-lambda";
+// import { StaticSite } from "../constructs/static-site";
+// import { AdminAuthFlow } from "../constructs/admin-auth-flow";
+// import { WebClientAuthFlow } from "../constructs/client-web-auth-flow";
+// import { ImageProcessorLambda } from "../constructs/image-processor-lambda";
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const adminSiteProps = createStaticSiteProps("admin-portal");
-    new StaticSite(this, "admin-site", adminSiteProps);
+    // const adminSiteProps = createStaticSiteProps("admin-portal");
+    // new StaticSite(this, "admin-site", adminSiteProps);
 
-    const webPlatformProps = createStaticSiteProps("client-web");
-    new StaticSite(this, "client-web", webPlatformProps);
+    // const webPlatformProps = createStaticSiteProps("client-web");
+    // new StaticSite(this, "client-web", webPlatformProps);
 
-    new AdminAuthFlow(this, "admin-auth-flow");
+    // new AdminAuthFlow(this, "admin-auth-flow");
 
-    new WebClientAuthFlow(this, "web-client-auth-flow");
+    // new WebClientAuthFlow(this, "web-client-auth-flow");
 
-    new ImageProcessorLambda(this, "image-processor", {
-      codeDirectory: "image_processor",
-    });
+    // new ImageProcessorLambda(this, "image-processor", {
+    //   codeDirectory: "image_processor",
+    // });
   }
 }
