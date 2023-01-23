081a474a39299a09e011d88cb7bb0cb302cd8443
jviloria <jviloria96744@gmail.com>
2023-01-23 14:31:59 -0800

    Change name of ping function

--- lambdas/app_server/routes/ping.py
+++ lambdas/app_server/routes/ping.py
@@ -3,5 +3,5 @@ from aws_lambda_powertools.event_handler.api_gateway import Router
 router = Router()
 
 @router.get("/ping")
-def get_user():
+def get_ping():
     return { "response": "I am alive"}
