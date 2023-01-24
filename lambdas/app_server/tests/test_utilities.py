6da8c3ba1fe312a4b7cb0a29fb0662a249e008b2
jviloria <jviloria96744@gmail.com>
2023-01-23 16:45:28 -0800

    Add unit tests for app_server, add job in pipeline to run app server tests

---
+++ lambdas/app_server/tests/test_utilities.py
@@ -0,0 +1,42 @@
+import pytest
+import utils.utilities as utils
+
+def test_get_iso_timestamp():
+    assert utils.get_iso_timestamp(1674171127611) == '2023-01-19T23:32:07.611000'
+
+
+@pytest.mark.parametrize("epoch_milliseconds, expected", [
+    (-10, Exception),
+    ("abc", Exception),
+    (None, Exception)
+])
+def test_get_iso_timestamp_error(epoch_milliseconds, expected):
+    with pytest.raises(expected):
+        utils.get_iso_timestamp(epoch_milliseconds)
+
+
+@pytest.fixture
+def user_post_event():
+    return {
+        "requestContext": {
+            "authorizer": {
+                "claims": {
+                    "cognito:username": "test_user_id",
+                    "email": "test_username"
+                }
+            }
+        }
+    }
+
+def test_get_user_data_from_event(user_post_event: dict):
+    user_data = utils.get_user_data_from_event(user_post_event)
+    assert len(user_data) == 2 
+
+
+@pytest.mark.parametrize("user_data_index, expected", [
+    (0, "test_user_id"),
+    (1, "test_username"),
+])
+def test_get_user_data_from_event_correct_data(user_data_index: int, expected: str, user_post_event: dict):
+    user_data = utils.get_user_data_from_event(user_post_event)
+    assert user_data[user_data_index] == expected
--- lambdas/app_server/utils/utilities.py
+++ lambdas/app_server/utils/utilities.py
@@ -1,12 +1,25 @@
+from utils.logger import logger
 from datetime import datetime
 
 def get_iso_timestamp(epoch_milliseconds: int) -> str:
-    epoch_seconds = epoch_milliseconds / 1000
-    return datetime.utcfromtimestamp(epoch_seconds).isoformat()
+    try:
+        if epoch_milliseconds < 0:
+            raise
+        epoch_seconds = epoch_milliseconds / 1000
+        return datetime.utcfromtimestamp(epoch_seconds).isoformat()
+    except Exception:
+        logger.exception("Invalid Timestamp Input", extra={"epoch_milliseconds": epoch_milliseconds})
+        raise Exception
 
 def get_user_data_from_event(event: dict) -> list[str]:
-    cognito_object = event["requestContext"]["authorizer"]["claims"]
-    user_id = cognito_object["cognito:username"]
-    username = cognito_object["email"]
+    try:
+        cognito_object = event["requestContext"]["authorizer"]["claims"]
+        user_id = cognito_object["cognito:username"]
+        username = cognito_object["email"]
 
-    return user_id, username
+        return user_id, username
+    except Exception:
+        logger.exception("Missing Cognito User Data", extra={
+            "authorizer": event
+        })
+        raise Exception
