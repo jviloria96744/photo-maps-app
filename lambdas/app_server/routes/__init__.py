177726b20d598f3ac02ccca509000cb7c552fa68
jviloria <jviloria96744@gmail.com>
2023-01-25 17:24:06 -0800

    Forgot to add photo router to list of routers that are included

--- lambdas/app_server/routes/__init__.py
+++ lambdas/app_server/routes/__init__.py
@@ -1,4 +1,5 @@
 from routes.user import router as user_router
+from routes.photo import router as photo_router
 from routes.ping import router as ping_router
 
-routers = [user_router, ping_router]
+routers = [user_router, photo_router, ping_router]
