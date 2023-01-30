608917a964ad819cf22c402715816957b80cf80a
jviloria <jviloria96744@gmail.com>
2023-01-29 23:30:06 -0800

    Uncomment rest of deploy step, comment all resources to deploy empty stack

--- cdk-stacks/app/bin/app.ts
+++ cdk-stacks/app/bin/app.ts
@@ -4,7 +4,7 @@ import * as cdk from "aws-cdk-lib";
 import { AppStack } from "../lib/app-stack";
 
 const app = new cdk.App();
-new AppStack(app, "AppStack", {
+new AppStack(app, "App", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
--- cdk-stacks/app/lib/app-stack.ts
+++ cdk-stacks/app/lib/app-stack.ts
@@ -1,47 +1,47 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { DynamoDBTable } from "../constructs/dynamo-db-table";
-import { AdminSiteStack } from "./stacks/admin-site-stack";
-import { WebClientStack } from "./stacks/web-client-stack";
-import { ImageProcessorWorkflowStack } from "./stacks/image-processor-workflow-stack";
-import { AppApiStack } from "./stacks/app-api-stack";
-import { WebSocketStack } from "./stacks/websocket-stack";
+// import { DynamoDBTable } from "../constructs/dynamo-db-table";
+// import { AdminSiteStack } from "./stacks/admin-site-stack";
+// import { WebClientStack } from "./stacks/web-client-stack";
+// import { ImageProcessorWorkflowStack } from "./stacks/image-processor-workflow-stack";
+// import { AppApiStack } from "./stacks/app-api-stack";
+// import { WebSocketStack } from "./stacks/websocket-stack";
 
 export class AppStack extends cdk.Stack {
-  adminSiteStack: AdminSiteStack;
-  webClientStack: WebClientStack;
-  imageProcessorWorkflowStack: ImageProcessorWorkflowStack;
-  appApiStack: AppApiStack;
-  websocketStack: WebSocketStack;
+  // adminSiteStack: AdminSiteStack;
+  // webClientStack: WebClientStack;
+  // imageProcessorWorkflowStack: ImageProcessorWorkflowStack;
+  // appApiStack: AppApiStack;
+  // websocketStack: WebSocketStack;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const dynamoDB = new DynamoDBTable(this, "db-table");
+    // const dynamoDB = new DynamoDBTable(this, "db-table");
 
-    const adminSiteStack = new AdminSiteStack(this, "admin-site");
+    // const adminSiteStack = new AdminSiteStack(this, "admin-site");
 
-    const webClientStack = new WebClientStack(this, "web-client");
+    // const webClientStack = new WebClientStack(this, "web-client");
 
-    const imageProcessorWorkflowStack = new ImageProcessorWorkflowStack(
-      this,
-      "image-processor",
-      {
-        dynamoTable: dynamoDB.table,
-      }
-    );
+    // const imageProcessorWorkflowStack = new ImageProcessorWorkflowStack(
+    //   this,
+    //   "image-processor",
+    //   {
+    //     dynamoTable: dynamoDB.table,
+    //   }
+    // );
 
-    const appApiStack = new AppApiStack(this, "app-server", {
-      dynamoTable: dynamoDB.table,
-      assetBucket: imageProcessorWorkflowStack.assetBucket.bucket,
-      cognitoUserPool: webClientStack.webClientAuthFlow.userPool,
-    });
+    // const appApiStack = new AppApiStack(this, "app-server", {
+    //   dynamoTable: dynamoDB.table,
+    //   assetBucket: imageProcessorWorkflowStack.assetBucket.bucket,
+    //   cognitoUserPool: webClientStack.webClientAuthFlow.userPool,
+    // });
 
-    const websocketStack = new WebSocketStack(this, "websocket");
+    // const websocketStack = new WebSocketStack(this, "websocket");
 
-    this.adminSiteStack = adminSiteStack;
-    this.webClientStack = webClientStack;
-    this.imageProcessorWorkflowStack = imageProcessorWorkflowStack;
-    this.appApiStack = appApiStack;
-    this.websocketStack = websocketStack;
+    // this.adminSiteStack = adminSiteStack;
+    // this.webClientStack = webClientStack;
+    // this.imageProcessorWorkflowStack = imageProcessorWorkflowStack;
+    // this.appApiStack = appApiStack;
+    // this.websocketStack = websocketStack;
   }
 }
