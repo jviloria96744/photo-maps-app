ef8524d623fb3e20011e472d64af6e0bb736b6fd
jviloria <jviloria96744@gmail.com>
2023-02-06 16:27:35 -0800

    Add back delete trigger

--- cdk-stacks/app/lib/stacks/event-bus-stack.ts
+++
@@ -1,8 +0,0 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-
-export class EventBusStack extends cdk.NestedStack {
-  constructor(scope: Construct, id: string, props?: cdk.NestedStackProps) {
-    super(scope, id, props);
-  }
-}
