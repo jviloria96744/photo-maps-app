5caae003834508b0f0e63f63b4eac147115597ed
jviloria <jviloria96744@gmail.com>
2023-02-06 19:24:35 -0800

    Implement function to trigger step functions

---
+++ lambdas/step_function_orchestrator/app.py
@@ -0,0 +1,31 @@
+import os
+import json
+import boto3
+from aws_lambda_powertools import Logger
+
+logger = Logger(service=os.getenv("POWERTOOLS_SERVICE_NAME"), level=os.getenv("LOG_LEVEL"))
+
+
+client = boto3.client('stepfunctions')
+
+def get_event_metadata(event: dict):
+    try:
+        event_metadata = json.loads(event["Records"][0]["body"])
+        event_metadata = event_metadata["Records"][0]
+
+        s3_object_key = event_metadata["s3"]["object"]["key"]
+
+        return json.dumps({
+            "object_key": s3_object_key,
+        })
+    except Exception as e:
+        logger.debug(str(e), extra=event)
+        raise Exception("Error extracting metadata from event object")
+
+def lambda_handler(event, context):
+    event_metadata = get_event_metadata(event)
+    logger.info("Event", extra=event_metadata)
+    # response = client.start_execution(
+    #     stateMachineArn='${ExpressStateMachineArn}',
+    #     input=event_metadata
+    # )
