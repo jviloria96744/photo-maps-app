6940f937d763a8fc2503ea894aaf2614a59368d8
jviloria <jviloria96744@gmail.com>
2023-02-08 23:25:59 -0800

    Refactor step function construct to put more logic in helper functions

--- cdk-stacks/app/constructs/step-functions/image-upload-step-function.ts
+++ cdk-stacks/app/constructs/step-functions/image-upload-step-function.ts
@@ -6,6 +6,7 @@ import { Stack } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { PythonLambda } from "../python-lambda";
 import { JsonPath } from "aws-cdk-lib/aws-stepfunctions";
+import { IFunction } from "aws-cdk-lib/aws-lambda";
 
 interface ImageUploadStepFunctionProps {
   imageLabelFilterLambda: PythonLambda;
@@ -24,98 +25,26 @@ export class ImageUploadStepFunction extends Construct {
 
     const { imageLabelFilterLambda, imageGeotaggerLambda, dynamoTable } = props;
 
-    const getUploadManifest = new tasks.CallAwsService(
-      this,
-      "Get Photo Upload Manifest",
-      {
-        service: "s3",
-        action: "getObject",
-        parameters: {
-          "Bucket.$": "$.bucket_name",
-          "Key.$": "$.object_key",
-        },
-        iamResources: ["*"],
-        resultSelector: {
-          "manifestData.$": "States.StringToJson($.Body)",
-        },
-        resultPath: "$.result",
-        comment:
-          "Get manifest file from S3 to determine files that need to be processed",
-      }
+    const getUploadManifest = this.createUploadManifestTask();
+
+    const mapImages = this.createBaseMapTask();
+
+    const parallelImageProcessingTask = this.createBaseParallelTask();
+
+    const geoTaggingTask = this.createLambdaInvokeTask(
+      "Extract Geotag Data",
+      imageGeotaggerLambda.function,
+      "geoData"
     );
-
-    const mapImages = new step_function.Map(this, "Process Images", {
-      parameters: {
-        "Bucket.$": "$.bucket_name",
-        "imageId.$": "$$.Map.Item.Value",
-        "userId.$": "$.result.manifestData.userId",
-      },
-      itemsPath: "$.result.manifestData.imageIds",
-      resultPath: "$.result",
-      maxConcurrency: 0,
-    });
-
-    const parallelImageProcessingTask = new step_function.Parallel(
-      this,
-      "Run Image Processing Tasks",
-      {
-        comment:
-          "The Geotagging and Rekognition steps are independent, therefore can be run in parallel",
-        resultPath: "$.result",
-      }
-    );
-
-    const geoTaggingTask = new tasks.LambdaInvoke(this, "Extract Geotag Data", {
-      lambdaFunction: imageGeotaggerLambda.function,
-      retryOnServiceExceptions: false,
-      resultSelector: {
-        "geoData.$": "$.Payload",
-      },
-      resultPath: "$.result",
-    });
 
     parallelImageProcessingTask.branch(geoTaggingTask);
 
-    const rekognitionBranch = new tasks.CallAwsService(
-      this,
-      "Detect Image Labels",
-      {
-        service: "rekognition",
-        action: "detectLabels",
-        parameters: {
-          Features: ["GENERAL_LABELS"],
-          Image: {
-            S3Object: {
-              "Bucket.$": "$.Bucket",
-              "Name.$": "$.imageId",
-            },
-          },
-        },
-        iamResources: ["*"],
-        additionalIamStatements: [
-          new iam.PolicyStatement({
-            actions: ["s3:getObject"],
-            resources: ["*"],
-          }),
-        ],
-        resultSelector: {
-          "imageLabels.$": "$.Labels",
-        },
-        resultPath: "$.result",
-      }
-    );
+    const rekognitionBranch = this.createRekognitionTask();
 
-    const imageLabelFilterTask = new tasks.LambdaInvoke(
-      this,
+    const imageLabelFilterTask = this.createLambdaInvokeTask(
       "Filter Image Labels",
-      {
-        lambdaFunction: imageLabelFilterLambda.function,
-        retryOnServiceExceptions: false,
-        resultSelector: {
-          "labels.$": "$.Payload",
-        },
-        resultPath: "$.result",
-      }
+      imageLabelFilterLambda.function,
+      "labels"
     );
 
     rekognitionBranch.next(imageLabelFilterTask);
@@ -126,32 +55,7 @@ export class ImageUploadStepFunction extends Construct {
       this,
       "Write Image Metadata To DynamoDB",
       {
-        item: {
-          pk: tasks.DynamoAttributeValue.fromString(
-            JsonPath.stringAt("$.userId")
-          ),
-          sk: tasks.DynamoAttributeValue.fromString(
-            JsonPath.format(
-              "IMAGE_{}",
-              JsonPath.stringAt("$.result[0].result.geoData.image_id")
-            )
-          ),
-          datetime_created: tasks.DynamoAttributeValue.fromString(
-            JsonPath.stringAt("$$.State.EnteredTime")
-          ),
-          datetime_updated: tasks.DynamoAttributeValue.fromString(
-            JsonPath.stringAt("$$.State.EnteredTime")
-          ),
-          geo_data: tasks.DynamoAttributeValue.mapFromJsonPath(
-            "$.result[0].result.geoData"
-          ),
-          object_key: tasks.DynamoAttributeValue.fromString(
-            JsonPath.stringAt("$.imageId")
-          ),
-          image_labels: tasks.DynamoAttributeValue.fromString(
-            JsonPath.stringAt("$.result[1].result.labels")
-          ),
-        },
+        item: this.createDynamoDBItemParam(),
         table: dynamoTable,
         resultPath: "$.result",
       }
@@ -169,5 +73,113 @@ export class ImageUploadStepFunction extends Construct {
     });
 
     this.machine = machine;
+  }
+
+  createUploadManifestTask() {
+    return new tasks.CallAwsService(this, "Get Photo Upload Manifest", {
+      service: "s3",
+      action: "getObject",
+      parameters: {
+        "Bucket.$": "$.bucket_name",
+        "Key.$": "$.object_key",
+      },
+      iamResources: ["*"],
+      resultSelector: {
+        "manifestData.$": "States.StringToJson($.Body)",
+      },
+      resultPath: "$.result",
+      comment:
+        "Get manifest file from S3 to determine files that need to be processed",
+    });
+  }
+
+  createBaseMapTask() {
+    return new step_function.Map(this, "Process Images", {
+      parameters: {
+        "Bucket.$": "$.bucket_name",
+        "imageId.$": "$$.Map.Item.Value",
+        "userId.$": "$.result.manifestData.userId",
+      },
+      itemsPath: "$.result.manifestData.imageIds",
+      resultPath: "$.result",
+      maxConcurrency: 0,
+    });
+  }
+
+  createBaseParallelTask() {
+    return new step_function.Parallel(this, "Run Image Processing Tasks", {
+      comment:
+        "The Geotagging and Rekognition steps are independent, therefore can be run in parallel",
+      resultPath: "$.result",
+    });
+  }
+
+  createLambdaInvokeTask(
+    taskTitle: string,
+    lambdaFunction: IFunction,
+    resultSelectorKey: string
+  ) {
+    return new tasks.LambdaInvoke(this, taskTitle, {
+      lambdaFunction: lambdaFunction,
+      retryOnServiceExceptions: false,
+      resultSelector: {
+        [`${resultSelectorKey}.$`]: "$.Payload",
+      },
+      resultPath: "$.result",
+    });
+  }
+
+  createRekognitionTask() {
+    return new tasks.CallAwsService(this, "Detect Image Labels", {
+      service: "rekognition",
+      action: "detectLabels",
+      parameters: {
+        Features: ["GENERAL_LABELS"],
+        Image: {
+          S3Object: {
+            "Bucket.$": "$.Bucket",
+            "Name.$": "$.imageId",
+          },
+        },
+      },
+      iamResources: ["*"],
+      additionalIamStatements: [
+        new iam.PolicyStatement({
+          actions: ["s3:getObject"],
+          resources: ["*"],
+        }),
+      ],
+      resultSelector: {
+        "imageLabels.$": "$.Labels",
+      },
+      resultPath: "$.result",
+    });
+  }
+
+  createDynamoDBItemParam() {
+    return {
+      pk: tasks.DynamoAttributeValue.fromString(JsonPath.stringAt("$.userId")),
+      sk: tasks.DynamoAttributeValue.fromString(
+        JsonPath.format(
+          "IMAGE_{}",
+          JsonPath.stringAt("$.result[0].result.geoData.image_id")
+        )
+      ),
+      datetime_created: tasks.DynamoAttributeValue.fromString(
+        JsonPath.stringAt("$$.State.EnteredTime")
+      ),
+      datetime_updated: tasks.DynamoAttributeValue.fromString(
+        JsonPath.stringAt("$$.State.EnteredTime")
+      ),
+      geo_data: tasks.DynamoAttributeValue.mapFromJsonPath(
+        "$.result[0].result.geoData"
+      ),
+      object_key: tasks.DynamoAttributeValue.fromString(
+        JsonPath.stringAt("$.imageId")
+      ),
+      image_labels: tasks.DynamoAttributeValue.fromString(
+        JsonPath.stringAt("$.result[1].result.labels")
+      ),
+    };
   }
 }
