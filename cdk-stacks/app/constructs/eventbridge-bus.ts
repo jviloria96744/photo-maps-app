5ded2a39f0dd36a60b66d489ded2867ec878fdfa
jviloria <jviloria96744@gmail.com>
2023-02-09 21:23:18 -0800

    Add cloudwatch log rule to event bus

--- cdk-stacks/app/constructs/eventbridge-bus.ts
+++ cdk-stacks/app/constructs/eventbridge-bus.ts
@@ -1,6 +1,8 @@
 import * as events from "aws-cdk-lib/aws-events";
-import { Stack } from "aws-cdk-lib";
+import * as logs from "aws-cdk-lib/aws-logs";
+import { Stack, RemovalPolicy } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { CloudWatchLogGroup } from "aws-cdk-lib/aws-events-targets";
 
 export class EventBridgeBus extends Construct {
   eventBus: events.EventBus;
@@ -9,6 +11,16 @@ export class EventBridgeBus extends Construct {
 
     const eventBus = new events.EventBus(this, "AppEventBus", {
       eventBusName: "AppEventBus",
+    });
+
+    const logGroup = new logs.LogGroup(this, "EventBusLogGroup", {
+      retention: logs.RetentionDays.ONE_DAY,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const logGroupRule = new events.Rule(this, "EventBusLogRule", {
+      eventBus,
+      targets: [new CloudWatchLogGroup(logGroup)],
     });
 
     this.eventBus = eventBus;
