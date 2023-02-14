10b463f72c40232840278e683cdb6a09c1753dfa
jviloria <jviloria96744@gmail.com>
2023-02-13 17:43:35 -0800

    Add env prop to AdminPortal props

--- cdk-stacks/app/bin/admin-portal.ts
+++ cdk-stacks/app/bin/admin-portal.ts
@@ -8,6 +8,10 @@ import * as path from "path";
 const app = new cdk.App();
 
 const adminPortalStack = new AdminSiteStack(app, "AdminPortal", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
   siteDomain: `${DOMAIN_NAMES.ADMIN_PORTAL_SUBDOMAIN}.${DOMAIN_NAMES.TLD_NAME}`,
   pathName: path.resolve(
     CONFIG.environment.basePath,
