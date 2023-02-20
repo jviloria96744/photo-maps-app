7cb7cd34f501bafa873f328d5186a243d4f9a6b5
jviloria <jviloria96744@gmail.com>
2023-02-19 22:57:45 -0800

    Revert manual chunking logic

--- client-web/vite.config.ts
+++ client-web/vite.config.ts
@@ -1,15 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { dependencies } from "./package.json";
-
-function renderChunks(deps: Record<string, string>) {
-  let chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (["react", "react-dom"].includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,16 +11,5 @@ export default defineConfig({
   },
   define: {
     global: "window",
-  },
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          ...renderChunks(dependencies),
-        },
-      },
-    },
   },
 });
