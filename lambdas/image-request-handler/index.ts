496e7d43993bc58ddd5830bc734db87e88467597
jviloria <jviloria96744@gmail.com>
2023-02-23 11:38:31 -0800

    Add necessary dependency to image-request-handler directory

--- lambdas/image-request-handler/index.ts
+++ lambdas/image-request-handler/index.ts
@@ -1,4 +1,6 @@
-export const handler = async (event, context) => {
+import { Handler } from "aws-lambda";
+
+export const handler: Handler = async (event, context) => {
   const request = event.Records[0].cf.request;
 
   console.log(event);
