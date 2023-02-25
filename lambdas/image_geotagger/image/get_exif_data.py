9297365d2d1ae879f30f9f1399e9ab2d38a084ee
jviloria <jviloria96744@gmail.com>
2023-02-24 20:44:01 -0800

    Log S3 Response Object

--- lambdas/image_geotagger/image/get_exif_data.py
+++ lambdas/image_geotagger/image/get_exif_data.py
@@ -7,6 +7,7 @@ def get_exif_data_from_s3_image(bucket_name: str, key: str, s3_client):
 
     try:
         s3_response_object = s3_client.head_object(Bucket=bucket_name, Key=key)
+        logger.info("S3 Response Object", extra=s3_response_object)
         object_content = s3_response_object['Metadata']
         exif_data = {
             "lat": object_content["x-amz-meta-latitude"],
