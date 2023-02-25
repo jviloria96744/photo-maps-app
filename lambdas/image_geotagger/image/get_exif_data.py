7b084b89a437573cf335fed55a1befd0327c5d57
jviloria <jviloria96744@gmail.com>
2023-02-24 20:51:35 -0800

    Fix key names for values coming from metadata object, x-amz-meta not required

--- lambdas/image_geotagger/image/get_exif_data.py
+++ lambdas/image_geotagger/image/get_exif_data.py
@@ -7,14 +7,14 @@ def get_exif_data_from_s3_image(bucket_name: str, key: str, s3_client):
 
     try:
         s3_response_object = s3_client.head_object(Bucket=bucket_name, Key=key)
-        logger.info("S3 Response Object", extra=s3_response_object)
+        logger.debug("S3 Response Object", extra=s3_response_object)
         object_content = s3_response_object['Metadata']
         exif_data = {
-            "lat": object_content["x-amz-meta-latitude"],
-            "lng": object_content["x-amz-meta-longitude"],
-            "date": object_content["x-amz-meta-date"],
-            "image_width": object_content["x-amz-meta-width"],
-            "image_height": object_content["x-amz-meta-height"],
+            "lat": object_content["latitude"],
+            "lng": object_content["longitude"],
+            "date": object_content["date"],
+            "image_width": object_content["width"],
+            "image_height": object_content["height"],
         }
     except Exception:
         logger.exception("Error retrieving file from S3")
